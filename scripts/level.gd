04599a44243776f0324ac59c7bb25c961468527d
Uns4inted <lalanskyl@gmail.com>
2024-06-21 13:01:40 +0300

    Add Start Area scene, update level

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
+@onready var start = $Start
 @onready var player = $Player
 
 func _ready():
+	player.global_position = start.get_spawn_position()
+	
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		#trap.connect("touched_player", _on_trap_touched_player) ## old syntax
@@ -25,4 +27,4 @@ func _on_trap_touched_player():
 		
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_position()
